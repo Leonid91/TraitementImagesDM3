4409588a5378ba232023c9194e590ed4255a0197
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2021-12-04 23:56:35 +0100

    no-working ransac

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 def ShiTomasi():
     img_kp = cv2.goodFeaturesToTrack(img_gray,500,0.01,10)
@@ -70,6 +71,44 @@ def SIFT():
     plt.title('Best Matching Points')
     plt.imshow(result)
     plt.show()
+
+#data - un ensemble d'observations
+#modele - un modèle qui peut être ajusté à des données
+#n - le nombre minimum de données nécessaires pour ajuster le modèle
+#k - le nombre maximal d'itérations de l'algorithme
+#t - une valeur seuil pour déterminer si une donnée correspond à un modèle
+#d - le nombre de données proches des valeurs nécessaires pour faire valoir que le modèle correspond bien aux données
+def ransac(data, model, n, k, t, d):
+
+    iterator = 0
+    meilleur_modele = None
+    meilleur_ensemble_points = None
+    meilleure_erreur = 999999999999999999999999999999999999999999999999999999999999999999999999 # Doit être l'infini
+
+    while iterator < k:
+        for i in range(0, n):
+            points_aléatoires.append(random.choice(data))
+        modèle_possible = None # ???
+        ensemble_points = points_aléatoires
+
+        for point in points_aléatoires:
+            if point - modele_possible < t:
+                ensemble_points.append(point)
+        if len(ensemble_points) > d:
+            modèle_possible = None #???
+            erreur = None #???
+            if erreur < meilleure_erreur:
+                meilleur_modele = modèle_possible
+                meilleur_ensemble_points = ensemble_points
+                meilleure_erreur = erreur
+        iterator++s
+
+    # Return 
+    # meilleur_modèle - les paramètres du modèle qui correspondent 
+    # le mieux aux données (ou zéro si aucun bon modèle n'a été trouvé)
+    # meilleur_ensemble_points - données à partir desquelles ce modèle a été estimé
+    # meilleure_erreur - l'erreur de ce modèle par rapport aux données
+    return [meilleur_modele, meilleur_ensemble_points, meilleure_erreur]
 
 img = cv2.imread("Michelangelo/Michelangelo_ThecreationofAdam_1707x775.jpg")
 frag = cv2.imread("Michelangelo/frag_eroded/frag_eroded_0.png")
